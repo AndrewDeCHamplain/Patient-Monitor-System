32bfb7cc1fabe1402f28f7c641615fae7e0b0e93
JeffChapman <jeffchapman@cmail.carleton.ca>
2013-11-12 16:15:24 -0500

    Update MainWindow.java

--- Server/MainWindow.java
+++ Server/MainWindow.java
@@ -1,41 +1,40 @@
 import java.awt.*;
+import java.util.*;
 
 import javax.swing.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
 public class MainWindow implements ActionListener{
 	
-private JFrame Piframe_1;
-private Container contentPanePi_1;
-private JFrame Piframe_2;
-private Container contentPanePi_2;
+
 private Container contentPane;
 private JButton Connect;
 private JMenuItem quitItem;
-private JMenuItem quitItem1;
-private JMenuItem quitItem2;
-private JToggleButton Pi_soundButton_1;
-private JToggleButton Pi_soundButton_2;
-private int t1 = 0;
-private int t2 = 0;
+private JButton Cpi_1;
+private JButton Cpi_2;
+private JButton Cpi_3;
+private JButton Cpi_4;
+
+
+private Client_Pi CC_1;
+private Client_Pi CC_2;
+private Client_Pi CC_3;
+private Client_Pi CC_4;
+
+private int q = 0;
 private int x = 0;    //setup number of connected clients
 private Color unopened = Color.white;
 private Color connected = Color.green;
 private Color warn = Color.red;
-private int warning = 0;
+private BitSet warning;
 
 	public MainWindow()
 	{
-		
-		//MainWindow test = new MainWindow();
-		//test.SetupMainWindow();
-	//}
-	
-	//public void SetupMainWindow()
-	//{
+		//setup the bits of warning. 0000
+		warning = new BitSet(3);
+		//warning.set(2, 4);    //testing the warnings
 		
 		//setup the frame
 		JFrame frame = new JFrame("Server");
@@ -93,220 +92,217 @@ private int warning = 0;
 	    warnings.setBorder(BorderFactory.createLineBorder(Color.lightGray));
 	    warnings.setLayout(new GridLayout(2,2));
 	    
-	    JTextField pi_1 = new JTextField();
-	    warnings.add(pi_1);
-	    pi_1.setBackground(unopened);
-	    JTextField pi_2 = new JTextField();
-	    warnings.add(pi_2);
-	    pi_2.setBackground(unopened);
-	    JTextField pi_3 = new JTextField();
-	    warnings.add(pi_3); 
-	    pi_3.setBackground(unopened);
-	    JTextField pi_4 = new JTextField();
-	    warnings.add(pi_4);
-	    pi_4.setBackground(unopened);
+	    Cpi_1 = new JButton("Off");
+	    warnings.add(Cpi_1);
+	    Cpi_1.addActionListener(this);
+	    Cpi_1.setBackground(unopened);
+	    Cpi_2 = new JButton("Off");
+	    warnings.add(Cpi_2);
+	    Cpi_2.addActionListener(this);
+	    Cpi_2.setBackground(unopened);
+	    Cpi_3 = new JButton("Off");
+	    warnings.add(Cpi_3); 
+	    Cpi_3.addActionListener(this);
+	    Cpi_3.setBackground(unopened);
+	    Cpi_4 = new JButton("Off");
+	    warnings.add(Cpi_4);
+	    Cpi_4.addActionListener(this);
+	    Cpi_4.setBackground(unopened);
 	    if(x >= 1)
 	    {
-	    	if(warning == 0)
+	    	if(!warning.get(0))
 	    	{
-	        	pi_1.setBackground(connected);
-	        	//pi_1.setText("safe");
-	    	}else if(warnings == 1)
-	    	{
-	    		Pi_1.setBackground(warning);
-	    		pi_1.setText("warning!");
+	    		Cpi_1.setBackground(connected);
+	    		Cpi_1.setText("Safe");
+	    	}else {
+	    		Cpi_1.setBackground(warn);
+	    		Cpi_1.setText("warning!");
 	    	}
 	    }
 	    if(x >= 2)
 	    {
-	        pi_2.setBackground(connected);
+	    	if(!warning.get(1))
+	    	{
+	    		Cpi_2.setBackground(connected);
+	    		Cpi_2.setText("Safe");
+	    	}else {
+	    		Cpi_2.setBackground(warn);
+	    		Cpi_2.setText("warning!");
+	    	}
+	        
 	    }
 	    if(x >= 3)
 	    {
-	        pi_3.setBackground(connected);
+	    	if(!warning.get(2))
+	    	{
+	    		Cpi_3.setBackground(connected);
+	    		Cpi_3.setText("Safe");
+	    	}else {
+	    		Cpi_3.setBackground(warn);
+	    		Cpi_3.setText("warning!");
+	    	}
 	    }
 	    if(x >= 4)
 	    {
-	        pi_4.setBackground(connected);
+	    	if(!warning.get(3))
+	    	{
+	    		Cpi_4.setBackground(connected);
+	    		Cpi_4.setText("Safe");
+	    	}else {
+	    		Cpi_4.setBackground(warn);
+	    		Cpi_4.setText("warning!");
+	    	}
 	    }
 	    //add the warnings to the pane
-	    contentPane.add(warnings);	
+	    contentPane.add(warnings);
+	    if(q == 1)
+	    {
+	    	Connect.setEnabled(false);
+	    }
 	}
 	
-	
-	public void SetupPi_1()
+	public void Set_Warnings(BitSet W)
 	{
-		Piframe_1 = new JFrame("Client 1");
-	    contentPanePi_1 = Piframe_1.getContentPane();
-	    contentPanePi_1.setLayout(new BoxLayout(contentPanePi_1, BoxLayout.Y_AXIS));
-	    Piframe_1.setBounds(250,0, 720, 1080);
-	    
-	    JMenuBar menubar1 = new JMenuBar();
-		Piframe_1.setJMenuBar(menubar1);
-	    
-	    JMenu fileMenu1 = new JMenu("Options"); //create menu
-		menubar1.add(fileMenu1);   //add menu
-		//add quit to the menu
-		quitItem1 = new JMenuItem("Quit");
-		fileMenu1.add(quitItem1);
+		warning.and(W);
+		setupMain();
+		if(!W.get(0))
+		{
+			ReplacePi(1);
+		}else if(!W.get(1))
+		{
+			ReplacePi(2);
+		}else if(!W.get(2))
+		{
+			ReplacePi(3);
+		}else if(!W.get(3))
+		{
+			ReplacePi(4);
+		}
 		
-		//setup quit
-		quitItem1.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-				
-			}
-		});
-	    
-	    JPanel Pi_vid_1 = new JPanel();
-	    Pi_vid_1.setBorder(BorderFactory.createLineBorder(Color.lightGray));
-	    Pi_vid_1.setSize(720, 480);
-	    contentPanePi_1.add(Pi_vid_1);
-	    Pi_vid_1.setBackground(Color.black);
-	    
-	    JPanel Pi_sound_1 = new JPanel();
-	    Pi_sound_1.setBorder(BorderFactory.createLineBorder(Color.lightGray));
-	    Pi_soundButton_1 = new JToggleButton("Play Sound");
-	    Pi_sound_1.add(Pi_soundButton_1);
-	    contentPanePi_1.add(Pi_sound_1);
-	    Pi_soundButton_1.addActionListener(this);
-	    
-	    JPanel Pi_sensors_1 = new JPanel();
-	    Pi_sensors_1.setBorder(BorderFactory.createLineBorder(Color.lightGray));
-	    Pi_sensors_1.setLayout(new BoxLayout(Pi_sensors_1, BoxLayout.X_AXIS));
-	    
-	    //Temp_count Temp = new Temp_count();
-	    //Pi_sensors_1.add(Temp);
-	    
-	    contentPanePi_1.add(Pi_sensors_1);
-	    
-	    Piframe_1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	    Piframe_1.pack();
-	    Piframe_1.setResizable(false);
-	    Piframe_1.setVisible(true);
 	}
 	
-	public void SetupPi_2()
+	
+	public void SetupPi(int z)
 	{
-		Piframe_2 = new JFrame("Client 2");
-	    contentPanePi_2 = Piframe_2.getContentPane();
-	    contentPanePi_2.setLayout(new BoxLayout(contentPanePi_2, BoxLayout.Y_AXIS));
-	    Piframe_2.setBounds(250,0, 720, 1080);
-	    
-	    JMenuBar menubar2 = new JMenuBar();
-		Piframe_2.setJMenuBar(menubar2);
-	    
-	    JMenu fileMenu2 = new JMenu("Options"); //create menu
-		menubar2.add(fileMenu2);   //add menu
-		//add quit to the menu
-		quitItem2 = new JMenuItem("Quit");
-		fileMenu2.add(quitItem2);
+		String l = "" + z;
+		System.out.println(l);
+		if(z == 1)
+		{
+			CC_1 = new Client_Pi(l);
+		}else if(z == 2)
+		{
+			CC_2 = new Client_Pi(l);
+		}else if(z == 3)
+		{
+			CC_3 = new Client_Pi(l);
+		}else if(z == 4)
+		{
+			CC_4 = new Client_Pi(l);
+		}
 		
-		//setup quit
-		quitItem2.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-				
-			}
-		});
-	    
-	    JPanel Pi_vid_2 = new JPanel();
-	    Pi_vid_2.setBorder(BorderFactory.createLineBorder(Color.lightGray));
-	    Pi_vid_2.setSize(720, 480);
-	    contentPanePi_2.add(Pi_vid_2);
-	    Pi_vid_2.setBackground(Color.black);
-	    
-	    JPanel Pi_sound_2 = new JPanel();
-	    Pi_sound_2.setBorder(BorderFactory.createLineBorder(Color.lightGray));
-	    Pi_soundButton_2 = new JToggleButton("Play Sound");
-	    Pi_sound_2.add(Pi_soundButton_2);
-	    contentPanePi_2.add(Pi_sound_2);
-	    Pi_soundButton_2.addActionListener(this);
-	    
-	    JPanel Pi_sensors_2 = new JPanel();
-	    Pi_sensors_2.setBorder(BorderFactory.createLineBorder(Color.lightGray));
-	    Pi_sensors_2.setLayout(new BoxLayout(Pi_sensors_2, BoxLayout.X_AXIS));
-	    
-	    //Temp_count Temp = new Temp_count();
-	    //Pi_sensors_1.add(Temp);
-	    
-	    contentPanePi_2.add(Pi_sensors_2);
-	    
-	    Piframe_2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	    Piframe_2.pack();
-	    Piframe_2.setResizable(false);
-	    Piframe_2.setVisible(true);
 	}
 	
+	public void ReplacePi(int h)
+	{
+		String s = "" + h;
+		System.out.println(s);
+		if(h == 1)
+		{
+			CC_1.ClearFrame();
+			CC_1.setWindow(s);
+		}else if(h == 2)
+		{
+			CC_2.ClearFrame();
+			CC_2.setWindow(s);
+		}else if(h == 3)
+		{
+			CC_3.ClearFrame();
+			CC_3.setWindow(s);
+		}else if(h == 4)
+		{
+			CC_4.ClearFrame();
+			CC_4.setWindow(s);
+		}
+	}
 	
-	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object o = e.getSource();
 		
 		if(o instanceof JButton)
 		{
-			//JButton b = (JButton)o; // if we want to use the button
+			JButton b = (JButton)o; // if we want to use more than one button
 			//rest of connect stuff here
-			x += 1;
-			setupMain();
-			System.out.println("button pressed.");
-			if(x == 1)
+			if(b.equals(Connect))
 			{
-				//MainWindow test1 = new MainWindow();
-				SetupPi_1();
-				/*
-				try {
-					Setup.Connect();
-				} catch (IOException e1) {
-					System.out.println("Failed to connect.");
-					e1.printStackTrace();
+				x += 1;
+				setupMain();
+				System.out.println("button pressed.");
+				if(x == 1)
+				{
+					//MainWindow test1 = new MainWindow();
+					//SetupPi_1();
+					SetupPi(1);
+				}else if(x == 2)
+				{
+					SetupPi(2);
+					//MainWindow test2 = new MainWindow();
+					//SetupPi_2();		
+				}else if(x == 3)
+				{
+					//warning.set(0, 2); 
+					//ReplacePi(1);    //testing reseting client 1		
+				}else
+				{
+					q = 1;
+					warning.set(0, 4); 
+					setupMain();
 				}
-				*/
-			}else if(x == 2)
+			}else if(b.equals(Cpi_1))  //if first client button
 			{
-				//MainWindow test2 = new MainWindow();
-				SetupPi_2();
-				
-			}else if(x == 3)
+				System.out.println("pressed warning button 1.");
+				if(warning.get(0))   // and if warning is on
+				{
+					System.out.println("reseting");
+					//reset warning
+					warning.clear(0);
+					//reset main window
+					setupMain();
+				}
+			}else if(b.equals(Cpi_2))  //if second client button
 			{
-				
-			}else
+				System.out.println("pressed warning button 2.");
+				if(warning.get(1))   // and if warning is on
+				{
+					System.out.println("reseting");
+					//reset warning
+					warning.clear(1);
+					//reset main window
+					setupMain();
+				}
+			}else if(b.equals(Cpi_3))  //if third client button
 			{
-				
+				System.out.println("pressed warning button 3.");
+				if(warning.get(2))   // and if warning is on
+				{
+					System.out.println("reseting");
+					//reset warning
+					warning.clear(2);
+					//reset main window
+					setupMain();
+				}
+			}else if(b.equals(Cpi_4))  //if fourth client button
+			{
+				System.out.println("pressed warning button 4.");
+				if(warning.get(3))   // and if warning is on
+				{
+					System.out.println("reseting");
+					//reset warning
+					warning.clear(3);
+					//reset main window
+					setupMain();
+				}
 			}
-			
-		}else if(o instanceof JToggleButton)
-		{
-			JToggleButton sound = (JToggleButton)o;
-			if(sound.equals(Pi_soundButton_1))
-			{
-				if(t1 == 0)
-				{
-					System.out.println("Client 1 Sound Toggled On");
-					t1 = 1;
-				}else 
-				{
-					System.out.println("Client 1 Sound Toggled Off");
-					t1 = 0;
-				}
-				
-			}else if(sound.equals(Pi_soundButton_2))
-			{
-				if(t2 == 0)
-				{
-					System.out.println("Client 2 Sound Toggled On");
-					t2 = 1;
-				}else 
-				{
-					System.out.println("Client 2 Sound Toggled Off");
-					t2 = 0;
-				}
-			}
-			//code to play music when we get that ready
 			
 		}
-		
 	}
 }
