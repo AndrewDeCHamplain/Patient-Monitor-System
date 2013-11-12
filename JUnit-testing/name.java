43fc8eb6bf1a1231565ed81c71b2fa1154e7ec73
Andrew DeChamplain <andrewdechamplain@cmail.carleton.ca>
2013-11-12 12:05:00 -0500

    Create name.java

---
+++ JUnit-testing/name.java
@@ -0,0 +1,7 @@
+public class Name
+{
+   private String first, last;
+   public Name(String first, String last) {}
+   public String toString() { return null; }
+   public boolean equals(Object o) { return false; }
+}
