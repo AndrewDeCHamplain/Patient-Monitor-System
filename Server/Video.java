e8ffa8f0b5af5c7d7285877582aebfa3e337eb99
JeffChapman <jeffchapman@cmail.carleton.ca>
2013-11-13 17:50:53 -0500

    Create Video.java

---
+++ Server/Video.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.media.*;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Video extends JPanel
+{
+	
+	private Player audioPlayer = null;
+	
+	public Video(URL url) throws NoPlayerException, CannotRealizeException, IOException
+	{
+		setLayout(new BorderLayout());
+		Start(url);
+	}	
+	
+	public void Start(URL url) throws NoPlayerException, CannotRealizeException, IOException
+	{
+		URL mediaURL = url;
+		audioPlayer = Manager.createRealizedPlayer(mediaURL);
+		
+		Component vid = audioPlayer.getVisualComponent();
+		add(vid, BorderLayout.CENTER);
+	}
+}
