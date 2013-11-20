7afe10d0b5168fc60a02a0e0a8e4d9433e5ae652
chrissparrow <chrissparrow@cmail.carleton.ca>
2013-11-20 18:06:21 -0500

    Added Heart Rate spoof
    Also a groin kick function, which should trigger the warning system on the server.

--- Server/Client.java
+++ Server/Client.java
@@ -5,24 +5,27 @@ import java.util.Random;
 
 
 public class Client {
-	
-		private static int bt = 37;
-		private static double CurrentBt = 37;
-		private static PrintWriter out;
+        
+                private static final int bt = 37;
+                private static double CurrentBt = 37;
+                private static final int hr = 70;
+                private static int CurrentHr = hr;
+                private static PrintWriter out;
+                private static BufferedReader in;
         
         static String IPaddress;
         //"134.117.58.12"
         
         public static void main(String args[]) throws UnknownHostException, IOException
         {
-        		IPaddress = args[0];
+                IPaddress = args[0];
                 Socket client = new Socket(IPaddress, 8081);
                 out = new PrintWriter(client.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
                 
                 @SuppressWarnings("unused")
-				String fromServer, toServer;
+                                String fromServer, toServer;
                 
                 while(client.isConnected())
                 {
@@ -37,45 +40,65 @@ public class Client {
                         }
                         if(toServer.equals("temp"))
                         {
-                        	sendTemp(50);
+                                sendTemp();
                         }
                 }
                 out.println("&discon&");
-                client.close();
                 System.out.println("Client was disconnected");
-                /*
-                if(client.isConnected())
-                {
-                        out.println("Hi");
-                }else
-                {
-                        System.out.println("No connection. unable to send.");
-                }
-                */
-               client.close();
+             
+                client.close();
         }
         
         public static double temperature()
         {
+                Random rand = new Random();
+                int m = rand.nextInt(50);
+                    double d = m/100.0;
+                if(bt > CurrentBt)
+                {
+                        return CurrentBt = CurrentBt + d;
+                }else
+                {
+                        return CurrentBt = CurrentBt - d;
+                }
+        }
+        
+        public static void sendTemp()
+        {    
+        	out.println("temp " + temperature());
+        }
+        
+        public static int heartRate()
+        {
         	Random rand = new Random();
-        	int m = rand.nextInt(50);
-    		double d = m/100.0;
-        	if(bt > CurrentBt)
-        	{
-        		return CurrentBt = CurrentBt + d;
-        	}else
-        	{
-        		return CurrentBt = CurrentBt - d;
+        	int rateChange = rand.nextInt(4);
+        	int sign = rand.nextInt(2);
+        	if (hr - 15 > CurrentHr) {
+        		return CurrentHr = CurrentHr + rateChange;
+        	}
+        	else if (hr + 15 < CurrentHr){
+        		return CurrentHr = CurrentHr - rateChange;
+        	}
+        	else {
+        		if(sign == 0){
+        			return CurrentHr = CurrentHr + rateChange;
+        		}
+        		else {
+        			return CurrentHr = CurrentHr - rateChange;
+        		}
         	}
         }
         
-        public static void sendTemp(int n)
+        public static void sendHeartRate() 
         {
-        	for(int i = n;i > 0; i--)
-        	{
-        		out.println("" + temperature());
-        	}
-        	
+        	out.println("HR " + heartRate());
         }
+        
+        public static void groinKick()
+        {
+        	CurrentHr = CurrentHr + 50;
+        	sendHeartRate();
+        }
+        
 
 }
