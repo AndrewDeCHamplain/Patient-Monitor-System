d7c6da3c6716cbd67cc9246ec6bc35ce1737069b
chrissparrow <chrissparrow@cmail.carleton.ca>
2013-11-25 13:41:47 -0500

    Update Client.java
    Added warnings

--- Server/Client.java
+++ Server/Client.java
@@ -19,6 +19,7 @@ public class Client {
         public static void main(String args[]) throws UnknownHostException, IOException
         {
                 IPaddress = args[0];
+                
                 Socket client = new Socket(IPaddress, 8081);
                 out = new PrintWriter(client.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(client.getInputStream()));
@@ -31,23 +32,31 @@ public class Client {
                 {
                         System.out.println(fromServer = in.readLine());
                         toServer = userIn.readLine();
+                        sendTemp();
+                        sendHeartRate();
+                        checkWarning();
+                        
                         if (toServer != null) {
                                 System.out.println("Client: " + toServer);
                                 out.println(toServer);
                         }
+                        
                         if (toServer.equals("disconnect")) {
                                 break;
                         }
+                        
                         if(toServer.equals("temp"))
                         {
                                 sendTemp();
                         }
+                       
                 }
                 out.println("&discon&");
                 System.out.println("Client was disconnected");
              
-                client.close();
+                client.close(); 
         }
+       
         
         public static double temperature()
         {
@@ -92,6 +101,31 @@ public class Client {
         public static void sendHeartRate() 
         {
         	out.println("HR " + heartRate());
+        }
+        
+        public static void checkWarning()
+        {
+        	boolean hrwarn = false , tempwarn = false;
+        	if (CurrentBt < bt - 1 || CurrentBt > bt + 1) {
+        		tempwarn = true;
+        	}
+        	if (CurrentHr < hr - 20 || CurrentHr > hr + 30) {
+        		hrwarn = true;
+        	}
+        	if(hrwarn || tempwarn) {
+        		if (hrwarn) {
+        			out.println("warning hr");
+        			System.out.println("Heart rate warning alert sent to server");
+        		}
+            	if (tempwarn) {
+            		out.println("warning temp");
+        			System.out.println("Temperature warning alert sent to server");
+            	}
+            	
+            }
+            else {
+            	out.println("warning clear");
+            }
         }
         
         public static void groinKick()
