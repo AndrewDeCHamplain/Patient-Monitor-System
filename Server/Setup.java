f9e7c17ffa4a8f682da3e5279ace2dd52b49c10b
chrissparrow <chrissparrow@cmail.carleton.ca>
2013-11-27 17:06:32 -0500

    Update Setup.java
    Formatting. Driving me nuts.

--- Server/Setup.java
+++ Server/Setup.java
@@ -5,64 +5,59 @@ import java.net.Socket;
 
 public class Setup
 {        
-		private static MainWindow gui;
-        private static ServerSocket server;
-        private static Socket client;
-        private static Socket client_1;
-        private static Socket client_2;
-        private static Socket client_3;
-        private static Socket client_4;
+	private static MainWindow gui;
+    private static ServerSocket server;
+    private static Socket client;
+    private static Socket client_1;
+    private static Socket client_2;
+    private static Socket client_3;
+    private static Socket client_4;
 
  
         
-        public static void main(String[] args) throws IOException
-        {
-                @SuppressWarnings("unused")
-				Setup hub = new Setup();
-        }
+    public static void main(String[] args) throws IOException
+    {
+        @SuppressWarnings("unused")
+		Setup hub = new Setup();
+    }
         
-        public Setup() throws IOException
-        {
-        	gui = new MainWindow();
-        	server = new ServerSocket(8081);
-        }
+    public Setup() throws IOException
+    {
+      	gui = new MainWindow();
+       	server = new ServerSocket(8081);
+    }
         
-        public static void Connect(int i, Client_Pi who) throws IOException
-        {
-
-        	client = server.accept();
-        	if(i == 2)
-        	{
-        		client_2 = client;
-        	}else if(i == 3)
-        	{
+    public static void Connect(int i, Client_Pi who) throws IOException
+    {
+       	client = server.accept();
+      	if(i == 2) {
+      		client_2 = client;
+       	}
+       	else if(i == 3) {
         		client_3 = client;
-        	}else if(i == 4)
-        	{
-        		client_4 = client;
-        	}else
-        	{
-            	client_1 = client;
-        	}
-        	new ClientThread(client, i, who, gui).start();
-        	
+       	}
+       	else if(i == 4) {
+        	client_4 = client;
         }
-        
-        public static void disconnect(int i) throws IOException
-        {
-        	if(i == 2)
-        	{
-        		client_2.close();
-        	}else if(i == 3)
-        	{
-        		client_3.close();
-        	}else if(i == 4)
-        	{
-        		client_4.close();
-        	}else
-        	{
-            	client_1.close();
-        	}
+        else {
+            client_1 = client;
         }
+       	new ClientThread(client, i, who, gui).start();
+	}
         
+    public static void disconnect(int i) throws IOException
+    {
+       	if(i == 2) {
+       		client_2.close();
+       	}
+       	else if(i == 3) {
+       		client_3.close();
+       	}
+       	else if(i == 4) {
+       		client_4.close();
+       	}
+       	else {
+           	client_1.close();
+       	}
+    }
 }
