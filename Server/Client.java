f903c5b8717c7547246937ff3ced82d4b98005e2
chrissparrow <chrissparrow@cmail.carleton.ca>
2013-11-28 12:21:51 -0500

    Update Client.java
    Changed the method of sending data to one string per cycle, to be parsed by ClientThread. Quite a few new methods, good for testing.

--- Server/Client.java
+++ Server/Client.java
@@ -6,8 +6,8 @@ import java.util.Random;
 
 public class Client {
         
-    private static final int bt = 37;
-    private static final int hr = 70;
+    private final int bt = 37;
+    private final int hr = 70;
     private PrintWriter out;
     private BufferedReader in;
     private double CurrentBt;
@@ -17,12 +17,12 @@ public class Client {
     //"134.117.58.12"
         
     public static void main(String args[]) throws UnknownHostException, IOException
-    {
-        //String IP = args[0];   
-     	Client c = new Client("");
+    {  
+     	Client c = new Client();
         c.run();
+        c.close();
     }
-    public Client(String IP) throws UnknownHostException, IOException
+    public Client() throws UnknownHostException, IOException
     {
         IPaddress = "localhost";
         CurrentBt = bt;
@@ -32,96 +32,80 @@ public class Client {
         in = new BufferedReader(new InputStreamReader(client.getInputStream()));
     }
         
-        public void run() throws IOException
-        {
-        	String fromServer;
-            
-            while(client.isConnected())
-            {
-                fromServer = in.readLine();
-                if(fromServer.equals("disconnect")) {
-                    System.out.println(fromServer);
-                    break;
-                }
-                else if (fromServer.equals("start")) {
-                    System.out.println(fromServer);
-                    sendTemp();
-                    sendHeartRate();
-                    checkWarning();
-                    System.out.println("sent");
-                }
-                else if(fromServer.equals("warning1")) {
-                        System.out.println(fromServer);
-                        bodyTempSpike();
-                        checkWarning();
-                        System.out.println("sent body temp spike");
-                }
-                else if(fromServer.equals("warning2")) {
-                        System.out.println(fromServer);
-                        heartRateSpike();
-                        checkWarning();
-                        System.out.println("sent heart rate spike");
-                }
-                else if(fromServer.equals("warning3")) {
-                        System.out.println(fromServer);
-                        heartRateSpike();
-                        bodyTempSpike();
-                        checkWarning();
-                        System.out.println("sent both spikes");
-                }
+    public void run() throws IOException
+    {
+    	String fromServer;
+           
+        while(client.isConnected()) {
+        	
+        	temperature(); 							//Refresh temperature value
+        	heartRate();							//Refresh Heart Rate value
+            fromServer = in.readLine();				//Check for message from server
+            if(fromServer.equals("disconnect")) {
+                System.out.println(fromServer);		
+                break;								//Stop running
             }
-            out.println("disconnect");
-            System.out.println("Client was disconnected");
-         
-            client.close();
+            else if (fromServer.equals("start")) {
+                System.out.println(fromServer);
+                statusUpdate();
+                System.out.println("sent");
+            }
+            else if(fromServer.equals("warning1")) {
+                System.out.println(fromServer);
+                bodyTempSpike();
+                statusUpdate();
+                System.out.println("sent body temp spike");
+            }
+            else if(fromServer.equals("warning2")) {
+                System.out.println(fromServer);
+                heartRateSpike();
+                statusUpdate();
+                System.out.println("sent heart rate spike");
+            }
+            else if(fromServer.equals("warning3")) {
+                System.out.println(fromServer);
+                heartRateSpike();
+                bodyTempSpike();
+                statusUpdate();
+                System.out.println("sent both spikes");
+            }
         }
-        private double temperature()
-        {
-                Random rand = new Random();
-                int m = rand.nextInt(50);
-                    double d = m/100.0;
-                if(bt > CurrentBt)
-                {
-                        return CurrentBt = CurrentBt + d;
-                }else
-                {
-                        return CurrentBt = CurrentBt - d;
-                }
+    }
+    private void temperature()
+    {
+        Random rand = new Random();
+        int m = rand.nextInt(50);
+        double d = m/100.0;
+        if(bt > CurrentBt) {
+        	CurrentBt = CurrentBt + d;
         }
-        
-        private void sendTemp() throws IOException
-        {    
-                out.println("temp " + temperature());
-               // wait(in.readLine().equals("go"));
+        else {
+        	CurrentBt = CurrentBt - d;
         }
-        
-        private int heartRate()
+    }
+    
+        private void heartRate()
         {
             Random rand = new Random();
             int rateChange = rand.nextInt(4);
             int sign = rand.nextInt(2);
             if (hr - 15 > CurrentHr) {
-                return CurrentHr = CurrentHr + rateChange;
+                CurrentHr = CurrentHr + rateChange;
             }
             else if (hr + 15 < CurrentHr){
-                return CurrentHr = CurrentHr - rateChange;
+                CurrentHr = CurrentHr - rateChange;
             }
             else {
                 if(sign == 0){
-                    return CurrentHr = CurrentHr + rateChange;
+                    CurrentHr = CurrentHr + rateChange;
                 }
                 else {
-                    return CurrentHr = CurrentHr - rateChange;
+                    CurrentHr = CurrentHr - rateChange;
                 }
             }
         }
-        
-        private void sendHeartRate() throws IOException 
-        {
-            out.println("HR " + heartRate());
-        }
-        
-        private void checkWarning()
+      
+        private String checkWarning()
         {
             boolean hrwarn = false , tempwarn = false;
             if (CurrentBt < bt - 1 || CurrentBt > bt + 1) {
@@ -130,23 +114,26 @@ public class Client {
             if (CurrentHr < hr - 20 || CurrentHr > hr + 30) {
                 hrwarn = true;
             }
+            
             if(hrwarn || tempwarn) {
                 if (hrwarn & !tempwarn) {
-                    out.println("warning hr");
-                    System.out.println("Heart rate warning alert sent to server");
+                    return "warning hr";
                 }
                 else if (tempwarn & !hrwarn) {
-                    out.println("warning temp");
-                    System.out.println("Temperature warning alert sent to server");
+                    return "warning temp";
                 }
-                else if (tempwarn & hrwarn) {
-                    out.println("warning both");
-                    System.out.println("Dual warning alert sent to server");
+                else {
+                    return "warning both";
                 }
-            }
+            } 
             else {
-                    out.println("warning clear");
+                return "warning clear";
             }
+        }
+        
+        private String statusUpdate()
+        {
+        	return (getCurrentHr() + getCurrentBt() + checkWarning());
         }
         
         private void heartRateSpike()
@@ -158,4 +145,22 @@ public class Client {
         {
                 CurrentBt = CurrentBt + 2;
         }
+        
+        public String getCurrentHr()
+        {
+        	return "hr " + CurrentHr + " ";
+        }
+        
+        public String getCurrentBt()
+        {
+        	return "temp " + String.format("%.2g%n", CurrentBt) + " ";
+        }
+        
+        public void close() throws IOException
+        {
+        	 out.println("disconnect");
+             System.out.println("Client was disconnected");
+             client.close();
+        }
+        
 }
