7935ab46dcccaf255768f696a20ce9380697a517
JeffChapman <jeffchapman@cmail.carleton.ca>
2013-12-07 18:26:00 -0500

    Update Client_Pi.java

--- Server_github/src/Client_Pi.java
+++ Server_github/src/Client_Pi.java
@@ -3,12 +3,6 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.io.IOException;
-import java.net.Socket;
-import java.util.BitSet;
-
-
-
-
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -35,28 +29,24 @@ public class Client_Pi extends JFrame {
         private JTextField hr;
         private JTextField panic;
         private JTextArea Label;
-        private int t = 0;
         private String Z;
-        private Socket socket;
         private int n;
-        private BitSet warn;
-        private BitSet warning;
+        private int port;
         private ClientThread thread;
         private EmbeddedMediaPlayerComponent mediaPlayerComponent;
         private String tempText;
         private String hrText;
         private MainWindow Main;
         
-        public Client_Pi(String z, BitSet warning, MainWindow main) 
+        public Client_Pi(String z, MainWindow main) 
         {
-        		this.Main = main;
-        		this.warning = warning;
-                n = Integer.parseInt(z);
-                Z = z;
-                CreateWindow(z);
-                warn = new BitSet(3);
+        	this.Main = main;
+            n = Integer.parseInt(z);
+            Z = z;
+            CreateWindow(z);
         }
         
+        //creates the frame
         public void CreateWindow(String Z)
         {
         	Piframe = new JFrame("Client " + Z);
@@ -64,6 +54,8 @@ public class Client_Pi extends JFrame {
         	tempText = "Connected";
         	hrText = "Connected";
         }
+        
+        //adds everything to the frame
         public void setWindow(String Z)
         {
             mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
@@ -95,41 +87,38 @@ public class Client_Pi extends JFrame {
             values.add(tempLabel);
             tempLabel.setEditable(false);
             
-             temp = new JTextField();
-             values.add(temp);
-             temp.setText(tempText);
-             temp.setHorizontalAlignment(JTextField.CENTER);
-             temp.setEditable(false);
+            temp = new JTextField();
+            values.add(temp);
+            temp.setText(tempText);
+            temp.setHorizontalAlignment(JTextField.CENTER);
+            temp.setEditable(false);
              
-             JTextField hrLabel = new JTextField();
-             hrLabel.setText("Heart Rate");
-             values.add(hrLabel);
-             hrLabel.setEditable(false);
+            JTextField hrLabel = new JTextField();
+            hrLabel.setText("Heart Rate");
+            values.add(hrLabel);
+            hrLabel.setEditable(false);
              
-             hr = new JTextField();
-             values.add(hr);
-             hr.setText(hrText);
-             hr.setHorizontalAlignment(JTextField.CENTER);
-             hr.setEditable(false);
+            hr = new JTextField();
+            values.add(hr);
+            hr.setText(hrText);
+            hr.setHorizontalAlignment(JTextField.CENTER);
+            hr.setEditable(false);
              
-             JTextField panicLabel = new JTextField();
-             panicLabel.setText("Panic Button");
-             values.add(panicLabel);
-             panicLabel.setEditable(false);
+            JTextField panicLabel = new JTextField();
+            panicLabel.setText("Panic Button");
+            values.add(panicLabel);
+            panicLabel.setEditable(false);
              
-              panic = new JTextField();
-              values.add(panic);
-              panic.setText("safe");
-              panic.setHorizontalAlignment(JTextField.CENTER);
-              panic.setEditable(false);
+            panic = new JTextField();
+            values.add(panic);
+            panic.setText("safe");
+            panic.setHorizontalAlignment(JTextField.CENTER);
+            panic.setEditable(false);
                         
-             bottom.add(values);
-             Label = new JTextArea();
-             //Label.append(" hello!\n");
-             //Label.append(" hello!\n");
-             //Label.append(" hello!");
-             Label.setEditable(false);
-             bottom.add(Label);
+            bottom.add(values);
+            Label = new JTextArea();
+            Label.setEditable(false);
+            bottom.add(Label);
              
             contentPanePi.add(bottom);
 
@@ -139,7 +128,16 @@ public class Client_Pi extends JFrame {
             Piframe.setVisible(true);
             videoPanel.setVisible(false);
             
-            mediaPlayerComponent.getMediaPlayer().playMedia("http://10.0.0.21:8080/?action=stream");
+            //set the window and wait for the url to be set by
+            //the thread
+            try {
+				hold();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+            
+            //System.out.println(URL + " " + port);
+            //mediaPlayerComponent.getMediaPlayer().playMedia("http://" + URL + ":" + port + "/?action=stream");
             //mediaPlayerComponent.getMediaPlayer().playMedia("http:/" + URL  );
             //mediaPlayerComponent.getMediaPlayer().playMedia("http://" + URL + "/?action=stream");
             //mediaPlayerComponent.getMediaPlayer().playMedia("http://hubblesource.stsci.edu/sources/video/clips/details/images/hst_1.mpg");
@@ -147,15 +145,32 @@ public class Client_Pi extends JFrame {
             
         }
         
+        //starts the video depending on wheither the client
+        //has a camera or not
+        public void startVideo(int c)
+        {
+        	if(c == 1)
+        	{
+        		System.out.println(URL + " " + port);
+        		mediaPlayerComponent.getMediaPlayer().playMedia("http://" + URL + ":8080/?action=stream");
+        	}else
+        	{
+        		//plays a video from the server
+        		mediaPlayerComponent.getMediaPlayer().playMedia("C:\\Crystallize-LindseyStirling.mp4");
+        	}
+        }
+        
+        //closes the client window
         public void CloseFrame() throws IOException
         {
             thread.stopConnection();
             mediaPlayerComponent.release();
             Piframe.setVisible(false);
             Piframe.dispose();
-            Setup.disconnect(n);
+            //Setup.disconnect(n);
         }
         
+        //clear this frame, repaint and reset the window
         public void ClearFrame()
         {
         	mediaPlayerComponent.release();
@@ -166,59 +181,42 @@ public class Client_Pi extends JFrame {
 			setWindow(Z);
         }
         
-/*
-            //sets the warning bits
-            //if q = 0. then clear the warnings
-            // if q = 1, then set the warning for this client
-        public BitSet Set(int q)
-        {
-        		warn = new BitSet(3);
-                if(q == 1)
-                {
-                       warning.set(n, true);
-                       return warn;
-                       
-                }else
-                {
-                	warning.set(n, false);
-                	return warning;
-                }
-        }
-        */
-        
+        //returns a reference to this client window
         public Client_Pi Who()
         {
                 return this;
         }
         
-        public void setSocket(Socket sock)
-        {
-                socket = sock;
-        }
-        
+        //set the thread for this client window
         public void setThread(ClientThread thread)
         {
                 this.thread = thread;
         }
         
-        public Socket GetSocket()
+        //set the port for this client window
+        public void setPort(int p, int c)
         {
-                return socket;
+        	System.out.println(p);
+        	port = p;
+        	//startVideo(c);
         }
         
+        //set the url for this client window
+        //add the patients data to the label
         public void setURL(String URL)
         {
+        	//System.out.println(URL);
         	this.URL = URL;
-            //String IP = URL.substring(1, 10);
-            //Label.append(IP);
-        	Label.append(Setup.parse(URL));
+        	setLabel(Setup.parse(URL));
         }
         
+        //set the label text
         public void setLabel(String lab)
         {
         	Label.append(lab);
         }
         
+        //set the temperature text
         public void setTempText(String in)
         {
         	in += "   ";
@@ -226,24 +224,28 @@ public class Client_Pi extends JFrame {
         	temp.setText(tempText);
         }
         
+        //set heart rate text
         public void setHRText(String in)
         {
         	hrText = in;
         	hr.setText(hrText);
         }
         
+        //set the temperature warning
         public void setTempWarn()
         {
         	temp.setBackground(new Color(209, 68, 68));
         	Main.Set_Warnings(n-1, 1);
         }
         
+        //set the heart rate warning
         public void setHRWarn()
         {
         	hr.setBackground(new Color(209, 68, 68));
         	Main.Set_Warnings(n-1, 1);
         }
         
+        //set the panic warning
         public void setPanicWarn()
         {
         	panic.setBackground(new Color(209, 68, 68));
@@ -251,49 +253,67 @@ public class Client_Pi extends JFrame {
         	Main.Set_Warnings(n-1, 1);
         }
         
-        public void clearTempWarn()
-        {
-        	temp.setBackground(Color.white);
-        }
-        
-        public void clearHRWarn()
-        {
-        	hr.setBackground(Color.white);
-        }
-        
-        public void clearPanic()
+        //clear the panic warning
+        public void clearPanicWarn()
         {
         	panic.setBackground(Color.white);
         	panic.setText("safe");
         }
         
+        //clear the temperature warning
+        public void clearTempWarn()
+        {
+        	temp.setBackground(Color.white);
+        }
+        
+        //clear the heart rate warning
+        public void clearHRWarn()
+        {
+        	hr.setBackground(Color.white);
+        }
+        
+        //returns the width of the screen
         public static int GetScreenWorkingWidth() {
             return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
         }
 
+        //returns the hight of the screen
         public static int GetScreenWorkingHeight() {
             return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
         }
         
+        //sets the location of the client's windows on
+        //the screen
         public void setLocationOnScreen()
         {
                 if(n == 1)
                 {
-                        Piframe.setLocation(250, 0);
+                	//top left
+                    Piframe.setLocation(250, 0);
                 }else if(n == 2)
                 {
-                        Piframe.setLocation((GetScreenWorkingWidth()/2)+250, 0);
+                	//top right
+                    Piframe.setLocation((GetScreenWorkingWidth()/2)+250, 0);
                 }else if(n == 3)
                 {
-                        Piframe.setLocation(250, (GetScreenWorkingHeight()/2));
+                	//bottom left
+                    Piframe.setLocation(250, (GetScreenWorkingHeight()/2));
                 }else if(n == 4)
                 {
-                        Piframe.setLocation((GetScreenWorkingWidth()/2)+250, (GetScreenWorkingHeight()/2));
+                	//bottom right
+                    Piframe.setLocation((GetScreenWorkingWidth()/2)+250, (GetScreenWorkingHeight()/2));
                 }
         }
         
+        //hold the thread for a short time
         public void hold() throws InterruptedException
         {
         	Thread.sleep(1000);
+        }
+        
+        //returns the number of this client
+        public int number()
+        {
+        	return n;
         }
 }
